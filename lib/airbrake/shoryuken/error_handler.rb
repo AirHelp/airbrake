cb1039ee5fb63e05b4f0770d2ef74ab854c3a40d
Pawel Niewiadomski <11110000b@gmail.com>
2016-11-04 15:28:19 +0100

    not triggering shoryuken for active jobs as that's already handled by other part

--- lib/airbrake/shoryuken/error_handler.rb
+++ lib/airbrake/shoryuken/error_handler.rb
@@ -7,7 +7,7 @@ module Airbrake
       def call(_worker, _queue, _sqs_msg, body)
         yield
       rescue Exception => exception
-        notify_airbrake(exception, body)
+        notify_airbrake(exception, body) unless active_job?(context['job_class'])
         raise exception
       end
       # rubocop:enable Lint/RescueException
@@ -17,6 +17,12 @@ module Airbrake
       def notify_airbrake(exception, context)
         params = context.merge(component: 'shoryuken', action: context['job_class'])
         Airbrake.notify(exception, params)
+      end
+
+      def active_job?(class_name)
+        activejob_class = "ActiveJob::Base".safe_constantize
+
+        !activejob_class.nil? && class_name.safe_constantize.new.kind_of?(activejob_class)
       end
     end
   end
